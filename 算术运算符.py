7e28788681ce6b19015a54086d59ff08fa9e373c
diaogit <71758840+diaogit@users.noreply.github.com>
2021-01-10 02:25:04 +0000

    Update 算术运算符.py

--- 算术运算符.py
+++ 算术运算符.py
@@ -14,3 +14,9 @@ a,b=7,5;
 #print(a<=b);
 #print(a>b);
 #print(a<b);
+#逻辑运算符 not,and,or
+#()>not>and>or
+print(a>b and c<d);
+print(not a>b);
+print(a>b or c>d);
+print(a>b and c<d or b<a and d>c or b<c and b>a);#优先级
