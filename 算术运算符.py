49278bd2c5b4f3a0bfdbbeba4041b3a0da43898a
diaogit <71758840+diaogit@users.noreply.github.com>
2021-01-10 10:10:39 +0800

    Add files via upload

---
+++ 算术运算符.py
@@ -0,0 +1,16 @@
+#+ - * / ** % // 算术运算符
+a,b=7,5;
+#print(a+b);
+#print(a-b);
+#print(a*b);
+#print(a/b);
+#print(a**b);
+#print(a%b);
+#print(a//b);
+#比较运算符 ==  !=  >=  <=  >  <
+#print(a==b);
+#print(a!=b);
+#print(a>=b);
+#print(a<=b);
+#print(a>b);
+#print(a<b);
