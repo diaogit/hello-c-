451c4fb1bbc90a78e3e970d8ed7ea16aeff00a9f
diaogit <71758840+diaogit@users.noreply.github.com>
2021-01-10 11:11:23 +0800

    Add files via upload

---
+++ python的输入输出.py
@@ -0,0 +1,9 @@
+#输出 %s  %d  %f
+#或者 {}format()
+#输入：input
+address=('北京');
+print("地址：%s"%(address));
+name=input("请输入你的姓名：");
+ipone=input("请输入你的手机号：");
+print('姓名：{}\n手机号：{}'.format(name,ipone));
+prin(type(ipone));
