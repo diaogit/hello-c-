c772080587dead99ab75cf6ea4a7a2b2bd8cc6fd
diaogit <71758840+diaogit@users.noreply.github.com>
2021-01-12 19:11:07 +0800

    Add files via upload

---
+++ 循环2.py
@@ -0,0 +1,69 @@
+                               #打印100以内的奇偶数
+#for data in range(1,101):
+#    if data%2==0:
+#        print("%d是偶数"%data,end=" ")
+#        pass
+#    else:
+#        print("%d是奇数"%data,end=" ")
+#        pass
+#    pass
+                                  #计算1加到100的和
+#sum=0
+#for num in range(1,101):
+#    sum+=num
+#    pass
+#print("1+...100=%d"%sum)
+                                    #break和continue语句
+#sum=0
+#for demo in range(1,101):
+#    sum+=demo
+#    if(sum>=100):
+#        break
+#    pass
+#print("当demo=%d时，累加和>=100"%demo)
+#pass
+#for demo in range(1,101):
+#    if(demo%2==0):
+#        continue
+#    else:
+#        print("%d是奇数！"%demo)
+#        pass
+#    pass
+#demo="i love you !"
+#for temp in demo:
+#    if(temp=='y'):
+#        continue
+#    print(temp,end='')
+#    pass
+                           #for循环打印99乘法表
+
+#for a in range(1,10):
+#    for b in range(1,a+1):
+#        print("%d*%d=%d"%(a,b,a*b),end=" ")
+#        pass
+#    a+=1
+#    print(" ")
+#    pass
+                        #验证密码
+#mima='123456'
+#zh='diaogit'
+#for a in range(3):
+#    c=input("请输入你的密码：")
+#    d=input("请输入你的账号：")
+#    if c==mima and d==zh:
+#        print('登陆成功!')
+#    break
+#    pass
+#else:
+#     print("你输入错误超过3次，请等待...")
+#pass
+
+        
+
+
+
+
+    
+
+
+
